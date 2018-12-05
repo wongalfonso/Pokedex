5f0f72f71910ff69b5c07e59857e6209b42befcc
wongalofonso <alfonsowongsd@gmail.com>
2018-12-04 22:50:18 -0800

    added home component

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 
 export default class App extends Component {
