e1f1cc79a35122e91b9f5fa6eee548dd461dbe96
wongalofonso <alfonsowongsd@gmail.com>
2018-12-04 22:23:40 -0800

    added react router and app component

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,2 +1,15 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import { BrowserRouter as Rouater, Switch, Route } from 'react-router-dom';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path = '/' component = { HomePage } />
+        </Switch>
+      </Router>
+    )
+  }
+}
